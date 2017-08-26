2ff47a6beff93b58501fb8c3f40ad6a5d04fb7d7
LeonMac <leon@LeonMacs-MacBook-Pro.local>
2017-08-26 18:13:40 +1200

    setup test env

---
+++ src/main/java/com/yuqiyu/querydsl/sample/chapter2/service/UserService.java
@@ -0,0 +1,35 @@
+package com.yuqiyu.querydsl.sample.chapter2.service;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.yuqiyu.querydsl.sample.chapter2.bean.QUser;
+import com.yuqiyu.querydsl.sample.chapter2.bean.QUserBean;
+import com.yuqiyu.querydsl.sample.chapter2.bean.User;
+import com.yuqiyu.querydsl.sample.chapter2.bean.UserBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    //查询工厂实体
+    private JPAQueryFactory queryFactory;
+
+    @PostConstruct
+    public void initFactory() {
+        System.out.println("开始实例化JPAQueryFactory");
+        queryFactory = new JPAQueryFactory(entityManager);
+    }
+
+    public List findAll(){
+        List<User> list = queryFactory.select(QUser.user).from(QUser.user).fetch();
+        return list;
+    }
+
+}
---
+++ src/test/java/com/yuqiyu/querydsl/sample/chapter2/ExampleNonTransactionalTests.java
@@ -0,0 +1,39 @@
+package com.yuqiyu.querydsl.sample.chapter2;
+
+import com.yuqiyu.querydsl.sample.chapter2.bean.User;
+import com.yuqiyu.querydsl.sample.chapter2.bean.UserBean;
+import com.yuqiyu.querydsl.sample.chapter2.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SpringBootTest(classes = UserService.class)
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+
+@Transactional
+public class ExampleNonTransactionalTests {
+
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void test1(){
+        User bean  = new User();
+        bean.setName("test");
+        entityManager.persist(bean);
+        System.out.println(userService.findAll());
+    }
+}
