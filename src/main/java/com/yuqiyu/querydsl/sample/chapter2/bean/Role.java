a8a13effebafc8cc6f3815903c4c4e6362281b44
gexiaolin <xiaolin.ge@cfgglobal.co.nz>
2017-08-26 17:37:51 +1200

    init project

---
+++ src/main/java/com/yuqiyu/querydsl/sample/chapter2/bean/Role.java
@@ -0,0 +1,18 @@
+package com.yuqiyu.querydsl.sample.chapter2.bean;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue
+    Long id;
+    String name;
+
+    @OneToMany(mappedBy = "role",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    List<User> users;
+}
---
+++ src/main/java/com/yuqiyu/querydsl/sample/chapter2/bean/User.java
@@ -0,0 +1,19 @@
+package com.yuqiyu.querydsl.sample.chapter2.bean;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    Long id;
+    String name;
+    String age;
+    @ManyToOne
+    @JoinColumn(name="role_id")
+    Role role;
+
+}
